d9941d8f5ca8a4981db82e62ea8f8cc08a27f316
DraganaBegojev <163642152+DraganaBegojev@users.noreply.github.com>
2025-11-17 08:06:07 +0100

    Define Model Associations
    -Add a one-to-many association between the User and Course models
    - Add a one-to-one association between the Course and User models
    - Set up the foreignKey property

--- models/user.js
+++ models/user.js
@@ -10,7 +10,12 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      User.hasMany(models.Course, {
+        foreignKey: {
+          fieldName: 'userId',
+          allowNull: false,
+        },
+      });
     }
   }
   User.init({
