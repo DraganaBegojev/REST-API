12b29bf384c2bccc1eea379bdc0229fc2793ac60
DraganaBegojev <163642152+DraganaBegojev@users.noreply.github.com>
2025-11-19 08:34:59 +0100

    Return a specific course including the User that owns the course
    /api/courses/:id - GET: Return the corresponding course including the User object associated with that course and a 200 HTTP status code.

--- routes.js
+++ routes.js
@@ -43,4 +43,17 @@ router.get('/courses', asyncHandler(async (req, res) => {
     res.status(200).json(courses);
 }));
 
+// Get /api/courses/:id - Returns a specific course including the User that owns the course
+router.get('/courses/:id', asyncHandler(async (req, res) => {
+    const course = await Course.findByPk(req.params.id, {
+        include: [{
+            model: User
+        }]
+    });
+    if (course) {
+        res.status(200).json(course);
+    } else {
+        res.status(404).json({ message: 'Course Not Found' });
+    }
+}));
 
